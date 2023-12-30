bb011965f99b02057d190a2b8cd7a6c5df5045e9
masudrana25 <mdmasudrana2512000@gmail.com>
2023-12-30 20:16:56 +0600

    dsfg

---
+++ 3. Constructor .py
@@ -0,0 +1,17 @@
+# constructor = a types of method = aita declared korer somoi e input parameter deya jai . extra declared korer por abar "set_value" korey parameter gula input deya lagy na.  
+# class er vitorey method er poribortey constructor use kora jai. 
+class Student :
+  roll = ''
+  gpa = ''
+
+  def __init__(self,roll,gpa) :
+    self.roll = roll; self.gpa = gpa
+  
+  def display(self): # methods or function in class
+    print(f"Roll : {self.roll}, GPA : {self.gpa}")
+
+rahim = Student(101,3.45)
+rahim.display()
+
+karim = Student(102,4.55)
+karim.display()
