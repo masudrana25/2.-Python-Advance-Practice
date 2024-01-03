3d07f68c3eaea51362e8af3d12b089d89102d804
masudrana25 <mdmasudrana2512000@gmail.com>
2024-01-03 12:47:28 +0600

    ADFHSjdhgfD

---
+++ 13. Creating own Module/13. Creating Own Module .py
@@ -0,0 +1,11 @@
+# Build-in module
+# from math import * # import all functions of math module
+# from math import pow, sqrt
+# print(pow(2,4)); print(sqrt(4))
+
+# own module
+from own_module import triangle_area, rectangle_area 
+# from own_module import *
+
+triangle_area(10,2)
+rectangle_area(10,20)
